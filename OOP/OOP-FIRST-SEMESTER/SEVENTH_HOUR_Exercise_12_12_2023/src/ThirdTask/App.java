06282a742c84203f7fb469b8fb665c3988cd2347
daniel <dani.dimitrov.snack9@gmail.com>
2024-01-04 01:34:49 +0200

    Update App.java

--- OOP/OOP-FIRST-SEMESTER/SEVENTH_HOUR_Exercise_12_12_2023/src/ThirdTask/App.java
+++ OOP/OOP-FIRST-SEMESTER/SEVENTH_HOUR_Exercise_12_12_2023/src/ThirdTask/App.java
@@ -5,7 +5,7 @@ public class App {
         Books book1 = new Books(23.22, "21345", "Sherloc Holmes", "Mystery", 15);
         Tv tv1 = new Tv(23.22, "21345", "Hitachi", "hyper", 150);
         System.out.println(book1.checkPromo());
-        System.out.println(tv1w.checkPromo());
+        System.out.println(tv1.checkPromo());
     }
 
 }
